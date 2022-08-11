f6682a7fed8b13fadb7f1d1fb89ab226396d9ccf
usa3amustafa <usa3a3ustafa@outlook.com>
2022-08-11 16:30:00 +0500

    about section

---
+++ main.js
@@ -0,0 +1,10 @@
+var loader = document.getElementById("loadbody");
+
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+
+document.getElementById("app").addEventListener("click", function(){
+    console.log("aaaaa")
+})

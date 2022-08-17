73281d41e7f65432a1c5699a7047dae170a1b9fe
usa3amustafa <usa3a3ustafa@outlook.com>
2022-08-17 12:31:51 +0500

    form

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ var loader = document.getElementById('loadbody')
 window.addEventListener('load', function () {
   loader.style.display = 'none'
 })
+
+// video
+
+const videos = document.querySelectorAll('.project-video')
+
+videos.forEach(vid => {
+  vid.addEventListener('mouseenter', e => {
+    vid.play()
+  })
+})
+videos.forEach(vid => {
+  vid.addEventListener('mouseleave', e => {
+    vid.pause()
+  })
+})
